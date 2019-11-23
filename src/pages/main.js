30959016e4d832d6f67704e5dcfaefea0f0bd145
GersonSalvador <zecoevane@yahoo.com.br>
2019-11-22 22:18:17 -0300

    Add pagination

--- src/pages/main.js
+++ src/pages/main.js
@@ -14,18 +14,30 @@ export default class Main extends Component {
   };
 
   state = {
+    productInfo: {},
     docs: [],
+    page: 1,
   };
 
   componentDidMount() {
     this.loadProducts();
   }
 
-  loadProducts = async () => {
-    const response = await api.get('/products');
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
 
-    const { docs } = response.data;
-    this.setState({ docs });
+    const { docs, ...productInfo } = response.data;
+    this.setState({ docs: [...this.state.docs, ...docs], page, productInfo });
+  };
+
+  loadMore = () => {
+    const { page, productInfo } = this.state;
+
+    if (page === productInfo.page) return;
+
+    const pageNumber = page + 1;
+    this.loadProducts(pageNumber);
+    this.state = { ...this.state, pageNumber };
   };
 
   renderItem = ({ item }) => (
@@ -46,6 +58,8 @@ export default class Main extends Component {
           data={this.state.docs}
           keyExtractor={item => item._id}
           renderItem={this.renderItem}
+          onEndReached={this.loadMore}
+          onEndReachedThreshold={0.1}
         />
       </View>
     );
